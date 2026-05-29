0f1950a7c7bb259588426deb0c59b2418b4ce9ca
David Glick <david@glicksoftware.com>
2026-05-29 09:42:48 +0200

    fix tests

--- src/plone/recyclebin/tests/conftest.py
+++ src/plone/recyclebin/tests/conftest.py
@@ -1,4 +1,3 @@
-from plone.recyclebin.testing import ACCEPTANCE_TESTING
 from plone.recyclebin.testing import FUNCTIONAL_TESTING
 from plone.recyclebin.testing import INTEGRATION_TESTING
 from pytest_plone import fixtures_factory
@@ -9,7 +8,6 @@ pytest_plugins = ["pytest_plone"]
 globals().update(
     fixtures_factory(
         (
-            (ACCEPTANCE_TESTING, "acceptance"),
             (FUNCTIONAL_TESTING, "functional"),
             (INTEGRATION_TESTING, "integration"),
         )
